595b2285529f81bd4ab2d96349053e7d2a603bcc
Su1008 <1281587427@qq.com>
2022-10-26 14:48:32 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,28 @@
+# Fake Yiban SU: 
+
+
+WARNING: This program is only used for demonstration and should not be abused. Use it at your own risk.
+
+警告：本程序仅供演示，请勿滥用。一切后果自负。
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
---
+++ dist/precache-manifest.9ac4ab213dadaed707d111f9096788cf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281407e33ef6289e8282",
+    "url": "/fake-yiban-su/dist/css/app.b12168d0.css"
+  },
+  {
+    "revision": "6d88906a58b583cf4c40fa78ff634f6b",
+    "url": "/fake-yiban-su/dist/img/about.6d88906a.svg"
+  },
+  {
+    "revision": "22956d4af7291008d7ef2a1a09933d64",
+    "url": "/fake-yiban-su/dist/img/add.22956d4a.svg"
+  },
+  {
+    "revision": "d4e6964a65886adb17196bc3b7f24b00",
+    "url": "/fake-yiban-su/dist/img/back.d4e6964a.svg"
+  },
+  {
+    "revision": "624da9341b407b128f61541f91342602",
+    "url": "/fake-yiban-su/dist/img/close.624da934.svg"
+  },
+  {
+    "revision": "3023f957506beb3ef1b630542a76eddb",
+    "url": "/fake-yiban-su/dist/img/delete.3023f957.svg"
+  },
+  {
+    "revision": "15eb1df8207605e82523c19b44520aed",
+    "url": "/fake-yiban-su/dist/img/logo.15eb1df8.png"
+  },
+  {
+    "revision": "11fc48501caa65d22a75dffb4d4c7bf4",
+    "url": "/fake-yiban-su/dist/img/menu.11fc4850.svg"
+  },
+  {
+    "revision": "285d673cb36271231c1becaa78d3fd77",
+    "url": "/fake-yiban-su/dist/img/qrcode0.285d673c.svg"
+  },
+  {
+    "revision": "970d6fbcd2c3388d424a7b60ab0dc63b",
+    "url": "/fake-yiban-su/dist/img/qrcode1.970d6fbc.svg"
+  },
+  {
+    "revision": "a22700c52873ef89f256d6e28a04397f",
+    "url": "/fake-yiban-su/dist/img/star_back.a22700c5.png"
+  },
+  {
+    "revision": "7f7af85a242ae5819325a9536d5dd721",
+    "url": "/fake-yiban-su/dist/index.html"
+  },
+  {
+    "revision": "281407e33ef6289e8282",
+    "url": "/fake-yiban-su/dist/js/app.fa93c60d.js"
+  },
+  {
+    "revision": "9e5e2dde2691847a6e9c",
+    "url": "/fake-yiban-su/dist/js/chunk-vendors.5c3755a9.js"
+  },
+  {
+    "revision": "d962cad20740c8cc61c4814769cf730c",
+    "url": "/fake-yiban-su/dist/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/fake-yiban-su/dist/robots.txt"
+  }
+]);
